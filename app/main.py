53ca6d7695370cfdac31c3f80d9ff2f000554e20
Martin Zlocha <mz4315@ic.ac.uk>
2018-01-27 14:27:14 +0000

    Initial structure.

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    # Only for debugging while developing
+    app.run(debug=True, port=80)
