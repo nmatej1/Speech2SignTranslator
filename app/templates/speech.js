2c51c73817460a3c78106580f70b59c44f85bc60
Matej Novak <nmatej1@Matejs-MacBook-Pro.local>
2018-01-27 14:41:20 +0000

    First commit with starting JS file

---
+++ app/templates/speech.js
@@ -0,0 +1,35 @@
+
+
+if (!('webkitSpeechRecognition' in window)) {
+  upgrade();
+} else {
+  var recognition = new webkitSpeechRecognition();
+  recognition.continuous = true;
+  recognition.interimResults = true;
+
+  recognition.onstart = function() { ... }
+  recognition.onresult = function(event) {
+    var interim_transcript = '';
+
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+      if (event.results[i].isFinal) {
+        final_transcript += event.results[i][0].transcript;
+      } else {
+        interim_transcript += event.results[i][0].transcript;
+      }
+    }
+    final_transcript = capitalize(final_transcript);
+    final_span.innerHTML = linebreak(final_transcript);
+    interim_span.innerHTML = linebreak(interim_transcript);
+  };
+  recognition.onerror = function(event) { ... }
+  recognition.onend = function() { ... }
+
+
+function startButton(event) {
+  ...
+  final_transcript = '';
+  recognition.lang = select_dialect.value;
+  recognition.start();
+
+
